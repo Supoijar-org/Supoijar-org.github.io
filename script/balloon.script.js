e4d457f59e16dcebb140081b8e2ae49bb9359312
HALKA9000-NIIP <csta19097@g.nihon-u.ac.jp>
2020-08-04 11:34:28 +0900

    style plus

---
+++ script/balloon.script.js
@@ -0,0 +1,24 @@
+function showBalloon() {
+  var wObjballoon = document.getElementById("makeImg");
+  if (wObjballoon.className == "balloon1") {
+    wObjballoon.className = "balloon";
+  } else {
+    wObjballoon.className = "balloon1";
+  }
+}
+
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var wObjballoonO = document.getElementById("balloonU");
+    wObjballoonO.addEventListener(
+      "mouseout",
+      () => {
+        var wObjballoon2 = document.getElementById("makeImg");
+        wObjballoon2.className = "balloon1";
+      },
+      false
+    );
+  },
+  false
+);
